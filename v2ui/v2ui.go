b7bbcbe485ef8952b519f284b4e6eb179add27f8
永雏塔菲 <108621198+taffychan@users.noreply.github.com>
2022-07-07 10:20:33 +0800

    Update v2ui.go

--- v2ui/v2ui.go
+++ v2ui/v2ui.go
@@ -12,16 +12,16 @@ import (
 func MigrateFromV2UI(dbPath string) error {
 	err := initDB(dbPath)
 	if err != nil {
-		return common.NewError("init v2-ui database failed:", err)
+		return common.NewError("init v2-ui database failed: ", err)
 	}
 	err = database.InitDB(config.GetDBPath())
 	if err != nil {
-		return common.NewError("init x-ui database failed:", err)
+		return common.NewError("init x-ui database failed: ", err)
 	}
 
 	v2Inbounds, err := getV2Inbounds()
 	if err != nil {
-		return common.NewError("get v2-ui inbounds failed:", err)
+		return common.NewError("get v2-ui inbounds failed: ", err)
 	}
 	if len(v2Inbounds) == 0 {
 		fmt.Println("migrate v2-ui inbounds success: 0")
@@ -31,7 +31,7 @@ func MigrateFromV2UI(dbPath string) error {
 	userService := service.UserService{}
 	user, err := userService.GetFirstUser()
 	if err != nil {
-		return common.NewError("get x-ui user failed:", err)
+		return common.NewError("get x-ui user failed: ", err)
 	}
 
 	inbounds := make([]*model.Inbound, 0)
@@ -42,7 +42,7 @@ func MigrateFromV2UI(dbPath string) error {
 	inboundService := service.InboundService{}
 	err = inboundService.AddInbounds(inbounds)
 	if err != nil {
-		return common.NewError("add x-ui inbounds failed:", err)
+		return common.NewError("add x-ui inbounds failed: ", err)
 	}
 
 	fmt.Println("migrate v2-ui inbounds success:", len(inbounds))
