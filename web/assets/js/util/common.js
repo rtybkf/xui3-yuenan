5bcb88b96e3f3ef370cdaeb62a2b5bc5ee6be4f2
永雏塔菲 <108621198+taffychan@users.noreply.github.com>
2022-07-09 22:18:34 +0800

    Update common.js

--- web/assets/js/util/common.js
+++ web/assets/js/util/common.js
@@ -56,13 +56,13 @@ function safeBase64(str) {
 
 function formatSecond(second) {
     if (second < 60) {
-        return second.toFixed(0) + ' Giây';
+        return second.toFixed(0) + ' 秒';
     } else if (second < 3600) {
-        return (second / 60).toFixed(0) + ' Phút';
+        return (second / 60).toFixed(0) + ' 分';
     } else if (second < 3600 * 24) {
-        return (second / 3600).toFixed(0) + ' Giờ';
+        return (second / 3600).toFixed(0) + ' 时';
     } else {
-        return (second / 3600 / 24).toFixed(0) + ' Ngày';
+        return (second / 3600 / 24).toFixed(0) + ' 日';
     }
 }
 
