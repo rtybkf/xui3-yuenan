40e3b998981d898f4b0664f5e1ad1922f112ca81
永雏塔菲 <108621198+taffychan@users.noreply.github.com>
2022-07-25 17:31:17 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -151,7 +151,7 @@ download_xui(){
     chmod +x x-ui bin/xray-linux-$(archAffix)
     cp -f x-ui.service /etc/systemd/system/
     
-    wget -N --no-check-certificate https://raw.githubusercontents.com/taffychan/x-ui/main/x-ui.sh -O /usr/bin/x-ui
+    wget -N --no-check-certificate https://raw.githubusercontent.com/taffychan/x-ui/main/x-ui.sh -O /usr/bin/x-ui
     chmod +x /usr/local/x-ui/x-ui.sh
     chmod +x /usr/bin/x-ui
 }
