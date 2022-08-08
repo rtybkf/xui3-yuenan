3cdc2177fc33cedfd0414a131913e4b7137083ae
永雏塔菲 <108621198+taffychan@users.noreply.github.com>
2022-08-08 20:21:43 +0800

    Update xray.js

--- web/assets/js/model/xray.js
+++ web/assets/js/model/xray.js
@@ -1264,7 +1264,15 @@ Inbound.VLESSSettings = class extends Inbound.Settings {
         this.decryption = decryption;
         this.fallbacks = fallbacks;
     }
+    
+    addVLESS() {
+        this.vlesses.push(new Inbound.VLESSSettings.VLESS());
+    }
 
+    delVLESS(index) {
+        this.vlesses.splice(index, 1);
+    }
+    
     addFallback() {
         this.fallbacks.push(new Inbound.VLESSSettings.Fallback());
     }
